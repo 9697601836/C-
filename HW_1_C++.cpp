3f994884eb4c9aa95d111f61912cdfe125f171e3
9697601836 <vanya_bykov_1996@mail.ru>
2022-10-02 21:51:59 +0300

    add HW_1_C++

---
+++ HW_1_C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+// - Сайт по продаже недвижимости. Переменная содержит стоимость квартиры.
+    int apartmentPrice = 5000000;
+// - Программа для учета товара в обувном магазине. Переменная содержит размер пары обуви.
+    int shoeSize = 45;
+// - Информационная система для посетителей зоопарка. Переменная содержит количество животных, которые находятся в зоопарке в данный момент.
+    int countAnimals = 77;
+// - Интернет магазин электроники. Переменная для цены нового товара.
+    int newProductPrice = 140000;
+  std::cout << "Значение apartmentPrice: " << apartmentPrice << "\n";
+  std::cout << "Значение shoesSize: " << shoeSize << "\n";
+  std::cout << "Значение countAnimals: " << countAnimals << "\n";
+  std::cout << "Значение newProductPrice: " << newProductPrice << "\n";
+}
